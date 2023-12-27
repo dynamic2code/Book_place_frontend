443aac4dcebf882a08672fbc38aa0cc2e3e05aac
dynamic2code <dynamicdesingns@gmail.com>
2023-12-27 21:11:25 +0300

    addition of hosted ip

--- book_place_vue/src/config.js
+++ book_place_vue/src/config.js
@@ -1,5 +1,6 @@
 const config = {
-    baseUrl: 'http://127.0.0.1:8000/api/v1',
+    baseUrl:'http://13.51.242.135/api/v1',
+    // baseUrl: 'http://127.0.0.1:8000/api/v1',
   };
   
   export default config;
