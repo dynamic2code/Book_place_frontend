3d84e25202e0946c07a613ee00814bcd31968921
dynamic2code <dynamicdesingns@gmail.com>
2023-12-28 09:29:28 +0300

    testing no-cors

--- book_place_vue/src/components/LoginComponent.vue
+++ book_place_vue/src/components/LoginComponent.vue
@@ -62,6 +62,7 @@
       console.log(url)
       const response = await fetch(url, {
         method: 'POST',
+        mode: 'no-cors',
         headers: {
           'Content-Type': 'application/json',
         },
